c0d0fa6a1b989be1d6c73c75f268498decad28f7
Vivek Singh <vivek.singh141@yahoo.com>
2022-11-22 12:56:06 +0530

    chore: testing ignore

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "vivektest" {
+resource "aws_s3_bucket" "vivektest1" {
   bucket = "examplebuckettftest"
   versioning {
     enabled = false
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "vivektest" {
   }
 }
 
-resource "aws_s3_bucket" "vivektest1" {
+resource "aws_s3_bucket" "vivektest2" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
